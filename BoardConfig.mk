468b8658a1cae7142d0fc108f3ebbc36292d1199
kleidione Freitas <kleidione@gmail.com>
2025-12-22 21:04:45 -0300

    miucamera-garnet: sepolicy: Centralize all camera-related policies in device tree
    Signed-off-by: Kleidione Freitas <kleidione@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,6 +11,3 @@ MIUICAMERA_PATH := device/xiaomi/miuicamera-garnet
 
 # Properties
 TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
-
-# Sepolicy
-BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
